2ec499e14fb9d37f0b571688ff694817750d9b01
Sameermd25 <149755097+Sameermd25@users.noreply.github.com>
2025-09-21 18:03:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+	let output="";
+	n=1;
+	while(n<=20){
+		if(n%3==0 && n%5==0){
+			output+="FizzBuzz\n"
+		}
+		else if(n%3==0){
+			output+="Fizz\n"
+		}
+		else if(n%5==0){
+			output+="Buzz\n"
+		}
+		else{
+			output+=n.toString()+"\n";
+		}
+		n++;
+	}
+	window.alert(output);
